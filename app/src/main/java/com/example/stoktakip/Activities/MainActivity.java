3518620a5c5a8b0be182ed5f9619ed5dd31a938f
mertbabur <mertbabur1998@gmail.com>
2020-11-04 00:35:22 +0300

    eklendi

--- app/src/main/java/com/example/stoktakip/Activities/MainActivity.java
+++ app/src/main/java/com/example/stoktakip/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.stoktakip;
+package com.example.stoktakip.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.stoktakip.R;
 
 public class MainActivity extends AppCompatActivity {
 
