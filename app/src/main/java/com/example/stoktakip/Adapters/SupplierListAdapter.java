ab21e84c0effd8f7cde37847c91bb26392f82a09
mertbabur <mertbabur1998@gmail.com>
2020-11-08 23:24:15 +0300

    müşteri listesi listeleme işlevi yerine getirildi .

--- app/src/main/java/com/example/stoktakip/Adapters/SupplierListAdapter.java
+++ app/src/main/java/com/example/stoktakip/Adapters/SupplierListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.stoktakip.Adapters;
 
 import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +10,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.stoktakip.Models.CustomerOrSupplier;
 import com.example.stoktakip.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -29,6 +36,7 @@ public class SupplierListAdapter extends RecyclerView.Adapter<SupplierListAdapte
 
         ImageView imageView_cardView_supplierList_supplierPP;
         TextView textView_cardView_supplierList_nameSurname, textView_cardView_supplierList_companyName;
+        CardView cardView_supplierList;
 
         public CardHolder(@NonNull View itemView) {
             super(itemView);
@@ -36,6 +44,7 @@ public class SupplierListAdapter extends RecyclerView.Adapter<SupplierListAdapte
             textView_cardView_supplierList_companyName = itemView.findViewById(R.id.textView_cardView_supplierList_companyName);
             textView_cardView_supplierList_nameSurname = itemView.findViewById(R.id.textView_cardView_supplierList_nameSurname);
             imageView_cardView_supplierList_supplierPP = itemView.findViewById(R.id.imageView_cardView_supplierList_supplierPP);
+            cardView_supplierList = itemView.findViewById(R.id.cardView_supplierList);
 
         }
     }
@@ -51,11 +60,33 @@ public class SupplierListAdapter extends RecyclerView.Adapter<SupplierListAdapte
     @Override
     public void onBindViewHolder(@NonNull CardHolder holder, int position) {
 
+        CustomerOrSupplier supplier = supplierList.get(position);
+        Log.e("asssdf", "girdi "+supplier.getName());
+        setInfoSupplier(holder, supplier);
+
     }
 
     @Override
     public int getItemCount() {
         return supplierList.size();
+    }
+
+
+    public void setInfoSupplier(final CardHolder holder, CustomerOrSupplier supplier){
+
+        holder.textView_cardView_supplierList_nameSurname.setText(supplier.getName() + " " + supplier.getSurname());
+        holder.textView_cardView_supplierList_companyName.setText(supplier.getCompanyName());
+
+        String userUID = FirebaseAuth.getInstance().getUid();
+
+        String photoKey = supplier.getPhoto();
+        FirebaseStorage.getInstance().getReference().child("SuppliersPictures").child(userUID).child(photoKey).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(holder.imageView_cardView_supplierList_supplierPP);
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/example/stoktakip/Fragments/SupplierListFragment.java
+++ app/src/main/java/com/example/stoktakip/Fragments/SupplierListFragment.java
@@ -12,10 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.stoktakip.Adapters.CustomerOrSupplierListAdapter;
 import com.example.stoktakip.Adapters.SupplierListAdapter;
 import com.example.stoktakip.Models.CustomerOrSupplier;
 import com.example.stoktakip.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +35,9 @@ public class SupplierListFragment extends Fragment {
 
     private SupplierListAdapter adapter;
 
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,11 +45,8 @@ public class SupplierListFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_supplier_list_design, container, false);
 
         defineAttributes(rootView);
-        supplierList.add(new CustomerOrSupplier("","","","","","",""));
-        supplierList.add(new CustomerOrSupplier("","","","","","",""));
-        supplierList.add(new CustomerOrSupplier("","","","","","",""));
 
-        defineRecyclerView();
+        getSupplierFromDB();
 
         return rootView;
 
@@ -55,6 +61,9 @@ public class SupplierListFragment extends Fragment {
         recyclerView_fragmentSupplierList = rootView.findViewById(R.id.recyclerView_fragmentSupplierList);
 
         supplierList = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
 
 
 
@@ -72,6 +81,46 @@ public class SupplierListFragment extends Fragment {
 
 
         recyclerView_fragmentSupplierList.setAdapter(adapter);
+
+    }
+
+
+
+    public void getSupplierFromDB(){
+
+        String userUID = FirebaseAuth.getInstance().getUid();
+
+        myRef.child("Suppliers").child(userUID).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                CustomerOrSupplier supplier = snapshot.getValue(CustomerOrSupplier.class);
+                supplierList.add(supplier);
+
+                defineRecyclerView();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
