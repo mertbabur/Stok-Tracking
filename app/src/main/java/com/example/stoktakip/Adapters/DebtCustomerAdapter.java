ef803fbfb2985e8f1aab0878951c455996f429af
mertbabur <mertbabur1998@gmail.com>
2020-12-10 00:58:11 +0300

    ödeme alma yapildi .

--- app/src/main/java/com/example/stoktakip/Adapters/DebtCustomerAdapter.java
+++ app/src/main/java/com/example/stoktakip/Adapters/DebtCustomerAdapter.java
@@ -1,14 +1,18 @@
 package com.example.stoktakip.Adapters;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +22,7 @@ import com.example.stoktakip.Models.CustomerOrSupplier;
 import com.example.stoktakip.R;
 import com.example.stoktakip.Utils.StockUtils;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -60,8 +65,10 @@ public class DebtCustomerAdapter extends RecyclerView.Adapter<DebtCustomerAdapte
     public class CardHolder extends RecyclerView.ViewHolder{
 
         ImageView imageView_cardView_debtCustomer_customerPP, imageView_cardView_debtCustomer_isPaid;
-        TextView textView_cardView_debtCustomer_companyName, textView_cardView_debtCustomer_customerName, textView_cardView_debtCustomer_totalDebt;
+        TextView textView_cardView_debtCustomer_companyName, textView_cardView_debtCustomer_customerName, textView_cardView_debtCustomer_totalDebt
+                , textView_cardView_debtCustomer_getPaid;
         CardView cardView_debtCustomer_customerClick;
+        TextInputEditText textInputEditText_alertView_getPaid_paidQuantity;
 
         public CardHolder(@NonNull View itemView) {
             super(itemView);
@@ -72,6 +79,7 @@ public class DebtCustomerAdapter extends RecyclerView.Adapter<DebtCustomerAdapte
             textView_cardView_debtCustomer_customerName = itemView.findViewById(R.id.textView_cardView_debtCustomer_customerName);
             textView_cardView_debtCustomer_totalDebt = itemView.findViewById(R.id.textView_cardView_debtCustomer_totalDebt);
             cardView_debtCustomer_customerClick = itemView.findViewById(R.id.cardView_debtCustomer_customerClick);
+            textView_cardView_debtCustomer_getPaid = itemView.findViewById(R.id.textView_cardView_debtCustomer_getPaid);
 
         }
     }
@@ -86,7 +94,7 @@ public class DebtCustomerAdapter extends RecyclerView.Adapter<DebtCustomerAdapte
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CardHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final CardHolder holder, int position) {
 
         final String debtCustomerKey = debtCustomerList.get(position);
 
@@ -99,6 +107,15 @@ public class DebtCustomerAdapter extends RecyclerView.Adapter<DebtCustomerAdapte
 
                 SoldProductlistFragment soldProductlistFragment = new SoldProductlistFragment();
                 StockUtils.gotoFragment(((AppCompatActivity)mContext), soldProductlistFragment, R.id.frameLayoutEntryActivity_holder, "debtCustomerKey", debtCustomerKey, 1);
+
+            }
+        });
+
+        holder.textView_cardView_debtCustomer_getPaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                createAlertViewForGetPaid(holder, debtCustomerKey);
 
             }
         });
@@ -177,11 +194,97 @@ public class DebtCustomerAdapter extends RecyclerView.Adapter<DebtCustomerAdapte
         Float debt = Float.valueOf(totalDebt);
 
         if (debt == 0){
-            holder.imageView_cardView_debtCustomer_isPaid.setImageDrawable(mContext.getResources().getDrawable(R.drawable.tik_icon));
+            changeColorImageAndSetEnabledFalseText(holder);
         }
 
     }
 
+    /**
+     * AlertView olusturur .
+     * getPaidFromCustomer metodunu cagirir . ---> odeme alir .
+     */
+    public void createAlertViewForGetPaid(final CardHolder holder, final String customerId){
+
+        View desing = ((AppCompatActivity)mContext).getLayoutInflater().inflate(R.layout.alertview_get_paid_design, null);
+
+        holder.textInputEditText_alertView_getPaid_paidQuantity = desing.findViewById(R.id.textInputEditText_alertView_getPaid_paidQuantity);
+
+        AlertDialog.Builder alertDialogbuilder = new AlertDialog.Builder(mContext);
+
+        alertDialogbuilder.setTitle("ÖDEME AL");
+        alertDialogbuilder.setMessage("Lütfen alınacak miktarı geçmeyecek şekilde tutarı giriniz ." );
+        alertDialogbuilder.setIcon(R.drawable.get_paid_icon);
+
+        alertDialogbuilder.setView(desing);
+
+        alertDialogbuilder.setPositiveButton("EVET", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                getPaidFromCustomer(holder, customerId);
+
+            }
+        });
+
+        alertDialogbuilder.setNegativeButton("HAYIR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+        });
+
+
+        alertDialogbuilder.create().show();
+
+    }
+
+
+    /**
+     * Musteriden odeme alir.
+     * Customers DB sindeki totalDebt i duzenler.
+     */
+    public void getPaidFromCustomer(final CardHolder holder, final String customerId){
+
+        final Float paidQuantity = Float.valueOf(holder.textInputEditText_alertView_getPaid_paidQuantity.getText().toString());
+
+        myRef.child("Customers").child(userUID).child(customerId).child("totalDebt").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Float totalDebt = Float.valueOf(snapshot.getValue().toString());
+                if (totalDebt >= paidQuantity) {
+                    totalDebt -= paidQuantity;
+
+                    myRef.child("Customers").child(userUID).child(customerId).child("totalDebt").setValue(String.valueOf(totalDebt));
+
+                    // kasaya alinan parayi aktar ... *** devam edielecek
+
+                    holder.textView_cardView_debtCustomer_totalDebt.setText(String.valueOf(totalDebt));
+
+                    if (totalDebt == 0.0)
+                        changeColorImageAndSetEnabledFalseText(holder);
+
+                }
+                else
+                    Toast.makeText(((AppCompatActivity)mContext), "Alınacak miktardan büyük değer girilemez .", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    public void changeColorImageAndSetEnabledFalseText(CardHolder holder){
+
+        holder.imageView_cardView_debtCustomer_isPaid.setImageDrawable(mContext.getResources().getDrawable(R.drawable.tik_icon));
+        holder.textView_cardView_debtCustomer_getPaid.setTextColor(Color.rgb(13,192,0));
+        holder.textView_cardView_debtCustomer_getPaid.setEnabled(false);
+
+    }
 
 }
 
--- app/src/main/java/com/example/stoktakip/Fragments/DetailCustomerOrSupplierFragment.java
+++ app/src/main/java/com/example/stoktakip/Fragments/DetailCustomerOrSupplierFragment.java
@@ -1,6 +1,7 @@
 package com.example.stoktakip.Fragments;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -11,15 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.stoktakip.Adapters.ProductListAdapter;
 import com.example.stoktakip.Adapters.SoldProductAdapter;
 import com.example.stoktakip.Adapters.SummaryProductAdapter;
 import com.example.stoktakip.Models.CustomerOrSupplier;
@@ -28,6 +30,7 @@ import com.example.stoktakip.Models.SoldProduct;
 import com.example.stoktakip.R;
 import com.example.stoktakip.Utils.StockUtils;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -47,8 +50,9 @@ public class DetailCustomerOrSupplierFragment extends Fragment {
                       , imageView_fragmentDetailCustomer_deleteCustomer, imageView_fragmentDetailCustomer_edit;
     private TextView textView_fragmentDetailCustomer_companyName, textView_fragmentDetailCustomer_customerName, textView_fragmentDetailCustomer_customerSurname
                      , textView_fragmentDetailCustomer_customerNum, textView_fragmentDetailCustomer_customerAddress, textView_fragmentDetailCustomer_totalDebt
-                     , textView_fragmentDetailCustomer_title;
+                     , textView_fragmentDetailCustomer_title, textView_fragmentDetailCustomer_getPaid;
     private RecyclerView recyclerViewfragmentDetailCustomer;
+    private TextInputEditText textInputEditText_alertView_getPaid_paidQuantity;
 
     private String CUSTOMER_OR_SUPPLIER_KEY;
     private String WHICH_BUTTON;
@@ -108,6 +112,7 @@ public class DetailCustomerOrSupplierFragment extends Fragment {
         recyclerViewfragmentDetailCustomer = rootView.findViewById(R.id.recyclerViewfragmentDetailCustomer);
         textView_fragmentDetailCustomer_totalDebt = rootView.findViewById(R.id.textView_fragmentDetailCustomer_totalDebt);
         textView_fragmentDetailCustomer_title = rootView.findViewById(R.id.textView_fragmentDetailCustomer_title);
+        textView_fragmentDetailCustomer_getPaid = rootView.findViewById(R.id.textView_fragmentDetailCustomer_getPaid);
 
         CUSTOMER_OR_SUPPLIER_KEY = getArguments().getString("customerOrSupplierKey", "bos customer or supplier key");
         WHICH_BUTTON = getArguments().getString("whichButton", "bos button ");
@@ -164,6 +169,16 @@ public class DetailCustomerOrSupplierFragment extends Fragment {
                     StockUtils.gotoFragment(getActivity(), addCustomerOrSupplierFragment, R.id.frameLayoutEntryActivity_holder, "whichButton", "editCustomerButton", "customerOrSupplierKey", CUSTOMER_OR_SUPPLIER_KEY, 1);
                 else
                     StockUtils.gotoFragment(getActivity(), addCustomerOrSupplierFragment, R.id.frameLayoutEntryActivity_holder, "whichButton", "editSupplierButton", "customerOrSupplierKey", CUSTOMER_OR_SUPPLIER_KEY, 1);
+
+            }
+        });
+
+        // odeme alma kismi ...
+        textView_fragmentDetailCustomer_getPaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                createAlertViewForGetPaid();
 
             }
         });
@@ -324,6 +339,9 @@ public class DetailCustomerOrSupplierFragment extends Fragment {
 
     }
 
+    /**
+     * Get Specific Supplier 's products from DB .
+     */
     public void getProductFromDB(){
 
         myRef.child("Suppliers").child(USER_UID).child(CUSTOMER_OR_SUPPLIER_KEY).child("ProductsKey").addChildEventListener(new ChildEventListener() {
@@ -358,6 +376,78 @@ public class DetailCustomerOrSupplierFragment extends Fragment {
             }
         });
 
+    }
+
+
+    /**
+     * AlertView olusturur .
+     * getPaidFromCustomer metodunu cagirir . ---> odeme alir .
+     */
+    public void createAlertViewForGetPaid(){
+
+        View desing = getLayoutInflater().inflate(R.layout.alertview_get_paid_design, null);
+
+        textInputEditText_alertView_getPaid_paidQuantity = desing.findViewById(R.id.textInputEditText_alertView_getPaid_paidQuantity);
+
+        AlertDialog.Builder alertDialogbuilder = new AlertDialog.Builder(getActivity());
+
+        alertDialogbuilder.setTitle("ÖDEME AL");
+        alertDialogbuilder.setMessage("Lütfen alınacak miktarı geçmeyecek şekilde tutarı giriniz ." );
+        alertDialogbuilder.setIcon(R.drawable.get_paid_icon);
+
+        alertDialogbuilder.setView(desing);
+
+        alertDialogbuilder.setPositiveButton("EVET", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                getPaidFromCustomer();
+
+            }
+        });
+
+        alertDialogbuilder.setNegativeButton("HAYIR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+        });
+
+
+        alertDialogbuilder.create().show();
+
+    }
+
+
+    /**
+     * Musteriden odeme alir.
+     * Customers DB sindeki totalDebt i duzenler.
+     */
+    public void getPaidFromCustomer(){
+
+        final Float paidQuantity = Float.valueOf(textInputEditText_alertView_getPaid_paidQuantity.getText().toString());
+
+        myRef.child("Customers").child(USER_UID).child(CUSTOMER_OR_SUPPLIER_KEY).child("totalDebt").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Float totalDebt = Float.valueOf(snapshot.getValue().toString());
+                if (totalDebt >= paidQuantity) {
+                    totalDebt -= paidQuantity;
+
+                    myRef.child("Customers").child(USER_UID).child(CUSTOMER_OR_SUPPLIER_KEY).child("totalDebt").setValue(String.valueOf(totalDebt));
+
+                }
+                else
+                    Toast.makeText(getActivity(), "Alınacak miktardan büyük değer girilemez .", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
